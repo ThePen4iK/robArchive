d5b1ad18f141a6157b3221996ce90d34216e6654
ThePen4iK <cloudbj1102@gmail.com>
2022-02-21 16:57:08 +0200

    start-screen with mobile menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -34,6 +34,28 @@
 })();
 document.addEventListener("DOMContentLoaded", function () {
 
+  const burger = document.querySelector(".hamburger"),
+        header = document.querySelector(".header"),
+        startScreen = document.querySelector('.js-start-screen'),
+        html = document.documentElement;
+        headerFixed = startScreen.offsetHeight;
+
+  window.onscroll = function () {
+    fixed();
+  }
+  function fixed() {
+    //&& window.pageYOffset >= headerFixed
+    if (window.scrollY > 0) {
+      header.classList.add("header__fixed");
+    } else {
+      header.classList.remove("header__fixed");
+    }
+  }
+  burger.addEventListener("click", ()=>{
+    html.classList.toggle("body-scroll");
+    header.classList.toggle("header--open");
+    burger.classList.toggle("hamburger--active");
+  })
 
 
 
