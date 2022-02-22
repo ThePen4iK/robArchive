a1ae9d1ccbc93ab3490dfa43fbcee72d5b7d9f05
ThePen4iK <cloudbj1102@gmail.com>
2022-02-22 17:31:23 +0200

    accordion not ready

--- app/js/accordion.js
+++ app/js/accordion.js
@@ -1,34 +1,42 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  // Тут можно добавить в массив классы, если еще есть аккордеон на сайте
-  const accordionBittonClasses = ['.prices__button'];
+    // Тут можно добавить в массив классы, если еще есть аккордеон на сайте
+    const accordionBittonClasses = ['.prices__button', '.accordion'];
 
-  const accordionButtons = document.querySelectorAll(accordionBittonClasses.join(','));
+    const accordionButtons = document.querySelectorAll(accordionBittonClasses.join(','));
 
-  accordionButtons.forEach(accordionButton => {
-    // Та штука, которая сворачивается/разворачивается. Она должна быть 
-    // следующим элементом в разметке после элемента, по которому кликают
-    const content = accordionButton.nextElementSibling;
+    accordionButtons.forEach((accordionButton, i) => {
+        // Та штука, которая сворачивается/разворачивается. Она должна быть
+        // следующим элементом в разметке после элемента, по которому кликают
+        const content = accordionButton.nextElementSibling;
 
-    if (content) {
-      const contentHeight = content.offsetHeight;
+        if (content) {
+            const contentHeight = content.offsetHeight;
 
-      content.style.height = 0;
+            content.style.height = 0;
 
-      content.classList.add('changed');
+            content.classList.add('changed');
 
-      accordionButton.addEventListener('click', () => {
-        if (accordionButton.classList.contains('active')) {
-          content.style.height = 0;
+            accordionButton.addEventListener('click', () => {
 
-          accordionButton.classList.remove('active');
-        } else {
-          content.style.height = contentHeight + 'px';
+                const accordionIcon = document.querySelectorAll(".accordion__icon");
+                accordionIcon.forEach((item, j) => {
+                    if (i === j) {
+                        item.classList.toggle("accordion__icon--active");
+                    }
+                })
 
-          accordionButton.classList.add('active');
+
+                if (accordionButton.classList.contains('active')) {
+                    content.style.height = 0;
+                    accordionButton.classList.remove('active');
+
+                } else {
+                    content.style.height = contentHeight + 'px';
+                    accordionButton.classList.add('active');
+                }
+            });
         }
-      });
-    }
-  });
+    });
 })();
